4948593f4c1a41534afbb447b59a01504f577f1c
Patrick Werner <pwerner2@gmail.com>
2016-10-14 14:12:08 -0400

    views rendering

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
-var app = {};
-
-app.Todo = Backbone.Model.extend({
+var Todo = Backbone.Model.extend({
   defaults: {
     title: '',
     completed: false
   }
 });
+
+var myTodo = new Todo({
+  title: "Check stuff in console. This is a test."
+});
+
+var TodoView = Backbone.View.extend({
+  tagname: 'li',
+    todoTpl: _.template($('#item-template').html()),
+    
+  events: {
+    'dblclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
+  },
+
+  initialize: function(){
+    this.$el = $('#todo');
+  },
+
+  render: function(){
+    this.$el.html(this.todoTpl(this.model.attributes));
+      this.input = this.$('.edit');
+      return this;
+  },
+
+  edit: function(){},
+
+  close: function(){},
+
+  updateOnEnter: function(e){}
+
+});
+
+var todoView = new TodoView({ model: myTodo});
